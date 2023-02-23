f254939cb577c3d09a4570470a9edde775242133
kruemmelbande <kai.scheufen@gmail.com>
2023-02-23 09:01:03 +0100

    some basic config handeling (will break, but idc)

--- schlafi.py
+++ schlafi.py
@@ -1,5 +1,46 @@
 import discord, pickle, json, datetime
 
+def cut(toCut,inputString):
+    if len(inputString)>=len(toCut):
+        if inputString[:len(toCut)]==toCut:
+            return inputString[len(toCut):]
+    return inputString
+
+def defaultConfig():
+    return {
+        "token": "token",
+        "prefix": "!",
+        "optional": {
+            "quote": "No quote set - schlafi, 2023",
+            "time": [0,0]
+        }
+    }
+
+def readFromConfig(key):
+    config=defaultConfig()
+    with open("config.json","r") as f:
+        tmpconfig=json.load(f)
+        for key in tmpconfig:
+            config[key]=tmpconfig[key]
+    if key in config:
+        return config[key]
+    else:
+        if key in config["optional"]:
+            return config["optional"][key]
+    
+def writeToConfig(key,value):
+    config=defaultConfig()
+    with open("config.json","r") as f:
+        tmpconfig=json.load(f)
+        for key in tmpconfig:
+            config[key]=tmpconfig[key]
+    if key in config["optional"]:
+        config["optional"][key]=value
+    else:
+        config[key]=value
+    with open("config.json","w") as f:
+        json.dump(config,f)
+
 class note:
     def __init__(self, authorID, content, due):
         self.authorID = authorID
@@ -25,7 +66,24 @@ class cHelp(command):
         for command in commands:
             out += commands[command].getInfo() + "\n"
         message.channel.send(out)
+
+class cQuote(command):
+    def __init__(self):
+        super().__init__("quote","Sets the quote.")
+        quote=readFromConfig("quote")
+    def execute(self,message):
+        c=message.contents
+        quote=cut(prefix+self.name,c).strip()
+        writeToConfig("quote",quote)
+    
         
+
+class cSetTime(command):
+    def __init__(self):
+        super().__init__("settime","Sets the time.")
+        self.time=[0,0]
+    def execute(self,message):
+        c=message.contents
 commands={
    "help": cHelp()
 }
